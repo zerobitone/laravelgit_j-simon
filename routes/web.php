e248925da90b24f3fa78d6859c9a291152e0b073
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2023-09-27 16:46:56 +0200

    uebung 7

--- routes/web.php
+++ routes/web.php
@@ -229,3 +229,68 @@ Route::resource('certificates',CertificateController::class)
 ->except(['index','edit','update'])
 //->only('store','create','show','destroy') // nur - whitelisting 
 ->names(['create' => 'certifikates.certify']);*/
+
+/* 
+uebung_07
+
+Übung 7: Der Query-String entscheidet über den Ausgang der Anfrage
+
+Erstelle eine Closure Route, die die URI routinglaravel.test/question handhabt.
+Abhängig von Parametern im Query-String soll eine Ausgabe wiedergegeben werden. 
+
+Die entsprechenden Anforderungen habe ich dir aufgelistet.
+
+Packe das Bild Success.png in deinem Laravel-Projekt in den Pfad ./public/assets/image/Success.png
+
+
+1. Wenn keine »id« vorhanden ist oder die »id« null ist, soll eine Fehlermeldung mit einem HTTP-Statuscode 404 zurückgegeben werden.
+
+2. Wenn der Parameter »file« true ist, also nicht null oder false, soll das Bild Success.png heruntergeladen werden. Der Name der Datei soll der Wert des »id«-Parameters + die Endung .png sein. Die Parameter »question« und »id« dürfen ebenfalls nicht null sein.
+
+3. Wenn die »question« nicht vorhanden ist, soll auf die Seite der Akademie "https://www.webmasters-fernakademie.de/" weitergeleitet werden. Die »id« soll aber vorhanden sein.
+
+4. Wenn die »question« sowie die »id« vorhanden sind, soll ein kleiner individueller Text wiedergegeben werden. Dieser soll bestätigen, dass die Frage gespeichert wurde. Der Statuscode soll 200 sein.
+
+Prüfe mit den nachfolgenden URLs, ob deine erwartete Ausgabe wiedergegeben wird.
+
+http://routinglaravel.test/question?question=Warum%20ist%20die%20Erde%20rund?&id=3&file=false
+
+http://routinglaravel.test/question?question=Warum%20ist%20die%20Erde%20rund?
+
+http://routinglaravel.test/question?question=Warum%20ist%20die%20Erde%20rund?&id=3&file=true
+
+http://routinglaravel.test/question?id=3&file=false
+
+*/
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Request;
+
+Route::get('question', function (Request $request) {
+	
+	if (!$request->filled('id')) { // keine id oder id ist null
+		echo "id=".$request->id;
+
+		return response('Ein Fehler ist aufgetreten, da keine id übergeben wurde', 404);//Response::HTTP_NOT_FOUND);
+
+	} elseif ($request->filled(['id', 'question']) && $request->file === "true") { // id, question file=true 
+
+		$id      = $request->id;
+		$newname = $id . ".png";
+
+		return response()->download('assets/image/Success.png', $newname);
+		//return response()->download('assets/image/Success.png', $newname)->deleteFileAfterSend(true);
+
+	} elseif ($request->has('id') && !$request->filled('question')) { // id, keine question 
+
+		return redirect()->away('https://www.webmasters-fernakademie.de');
+
+	} elseif ($request->filled(['id', 'question']) && $request->file !== "true") { // id,question, file=false 
+
+		return "Ihre Frage wurde erfolgreich gespeichert.";	
+		/*return response()->json([
+			'art' => "antwort",
+			'inhalt' => 'Ihre Frage wurde erfolgreich gespeichert.'
+		]);*/
+	}
+
+});
