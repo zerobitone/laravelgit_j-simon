83d0fb0d3f77cb47e68d75073825e1c6ea8e6c7a
Jens Simon <64278468+jens-simon@users.noreply.github.com>
2023-10-03 15:45:56 +0200

    Übung 17 , RAW-Queries mit der interests table

--- app/Http/Controllers/InterestController.php
+++ app/Http/Controllers/InterestController.php
@@ -12,14 +12,15 @@ class InterestController extends Controller
     public function index()
     {
         echo "index";
-        
+
 
         $interests = DB::select('select * from interests ');
         dump($interests);
-        foreach($interests as $interest)
+        foreach ($interests as $interest)
             var_dump($interest);
-
-   
+        echo "<br><br>Ausgabe der kompletten Tabelle<br>"; // Ausgabe aller Felder
+        foreach ($interests as $interest)
+            echo $interest->id . ", " . $interest->text . ", " . ($interest->created_at ?: "null") . ", " . ($interest->updated_at ?: "null") . "<br>";
         /*
         dump(DB::table('interests')->dump());
         //dump(DB::table('interests')->dd());
@@ -37,18 +38,26 @@ class InterestController extends Controller
     public function create($id, $text)
     {
         DB::listen(function ($sql) {
-            dump($sql);
+            dump($sql); // alle SQL Befehle mitprotokollieren
         });
 
-        DB::insert('insert into interests (id, text) values (:id, :text)', ['id' => $id, 'text' => $text]);
+        try {
+            DB::insert('insert into interests (id, text) values (:id, :text)', ['id' => $id, 'text' => $text]);
+        } catch (\Illuminate\Database\QueryException $ex) {
+            return "Ein Fehler ist aufgetreten: ".$ex->getMessage();
+            
+        }
+        return "Ein Interesse wurde eingefügt, bitte die Übersicht aufrufen zur Kontrolle";
     }
 
     public function delete($id)
     {
         DB::listen(function ($sql) {
-            dump($sql);
+            dump($sql); // alle SQL Befehle mitprotokollieren
         });
+
         $removed = DB::delete('DELETE FROM interests where id = ?', [$id]);
-        return dd($removed);
+        echo "removed: ".($removed?"ja":"nein");
+        return "<br>Der Datensatz wurde versucht zu löschen!";
     }
 }
